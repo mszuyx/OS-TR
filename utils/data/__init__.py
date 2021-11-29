6fe08dfed9846f18b436b53429e4f9ed0c7f5343
mszuyx <37651144+mszuyx@users.noreply.github.com>
2021-11-29 17:26:03 -0600

    Add files via upload

--- utils/data/__init__.py
+++ utils/data/__init__.py
@@ -1,8 +1,9 @@
 # from utils.data.pascal import mydataset_pascal
-from utils.data.my_dataset2 import mydataset_embedding
-
+from utils.data.my_dataset_dtd import dtd_embedding
+from utils.data.my_dataset_tcd import tcd_embedding
 
 datasets = {
     # 'pascal': mydataset_pascal,
-    'dtd': mydataset_embedding,
+    'dtd': dtd_embedding,
+    'tcd': tcd_embedding
 }
